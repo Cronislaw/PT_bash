0dac0295bbd53ee6da7f82b97873a54667818f90
Cronislaw <cronislaw@gmail.com>
2014-06-02 00:01:33 +0200

    Create zad5a.sh

---
+++ zad5a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -gt 0 ]]; then
+	for i in $@; do
+		if [[ -e $i ]]; then
+			cat $i
+		else
+			echo 'Plik nie istnieje.'
+		fi
+		echo -e "\n-"
+	done
+else
+	echo 'Brak parametrow.'
+fi
