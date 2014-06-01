017e443e68febac6cc5c002b25fdc06869991d44
Cronislaw <cronislaw@gmail.com>
2014-06-02 00:01:11 +0200

    Create zad4.sh

---
+++ zad4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# o to chodzilo?
+#./zadanie4.sh login_na_olimpie
+
+if [[ $# < 1 ]]; then
+	echo 'Za malo parametrow.'
+	exit $?
+fi
+
+tar -czf bash.tar.gz *
+if [[ -e bash.tar.gz ]]; then
+	scp bash.tar.gz $1@olimp.mif.pg.gda.pl:~
+fi
+
+who > zalogowani
+who | sort > zalogowani.alfabetycznie
+head -3 zalogowani.alfabetycznie > zalogowani.head
